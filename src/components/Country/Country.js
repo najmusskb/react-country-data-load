fd9a2d4e6adc0c280236b28a5492897a73797532
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-10-07 22:55:34 +0600

    country data load

---
+++ src/components/Country/Country.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Country = () => {
+
+    const country = useLoaderData()[0];
+
+    return (
+        <div>
+
+            <h1>name:{country.name.common}</h1>
+
+
+        </div>
+    );
+};
+
+export default Country;
